d7ffa0abe6fb94ee6987385a8bfa8c5f534248e6
Ahmad Yousuf <139548576+0xAhmadYousuf@users.noreply.github.com>
2026-07-27 01:05:49 +0600

    Create cache_middleware.py

---
+++ app/core/cache_middleware.py
@@ -0,0 +1,12 @@
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+
+class NoCacheHTMLMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        response = await call_next(request)
+        content_type = response.headers.get("content-type", "")
+        if "text/html" in content_type:
+            response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+            response.headers["Pragma"] = "no-cache"
+            response.headers["Expires"] = "0"
+        return response
